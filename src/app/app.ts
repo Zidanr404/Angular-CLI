9af5dfb53a5010f870873939dcf383ec5ae5f352
caua <julianacaua@gmail.com>
2026-04-28 17:08:03 -0300

    testando propriedades

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('introducao-angular');
+  exibeNavbar: boolean = false;
 }
